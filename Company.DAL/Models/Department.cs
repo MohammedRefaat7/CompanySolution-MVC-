cacdd4c0dff60e860666f6bffaf7f518a890f44e
MohammedRefaat7 <mr8024072@gmail.com>
2024-09-22 00:58:24 +0200

    AddEntities

---
+++ Company.DAL/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Models
+{
+    public class Department : BaseEntity
+    {
+        [Required(ErrorMessage = "Name Is Required")]
+        [MaxLength(55)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Code Is Required")]
+        public string Code { get; set; }
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
