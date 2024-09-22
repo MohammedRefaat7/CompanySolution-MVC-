539a58052e83e52527c8eb98b071b88e5c0a9d9d
MohammedRefaat7 <mr8024072@gmail.com>
2024-09-22 02:40:12 +0200

    DbContextClass

---
+++ Company.DAL/Migrations/CompanyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Migrations
+{
+    public class CompanyDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ; Database=CompanyDb ; Trusted_Connection =true ;");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
