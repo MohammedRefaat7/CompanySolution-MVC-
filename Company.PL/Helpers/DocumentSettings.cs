2c09f4c2b5952f078e0110db45a4ee280faced4e
MohammedRefaat7 <mr8024072@gmail.com>
2024-10-01 22:51:11 +0200

    DocumentSettings(UPLOAD)

---
+++ Company.PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace Company.PL.Helpers
+{
+    public static class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file , string FolderName)
+        {
+            // CurrentDirectory\\wwwroot\\Files\\FolderName    (FilePath)
+            string FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);
+
+            // Unique FileName
+            string FileName = $"{Guid.NewGuid()}{file.FileName}";
+
+            // FilePath(FolderPath , UFileName)
+            string FilePath = Path.Combine(FolderPath, FileName);
+
+            // Save file As STREAM 
+            using var Fs = new FileStream(FilePath, FileMode.Create);
+            file.CopyTo(Fs);
+
+            return FilePath;
+        }
+    }
+}
